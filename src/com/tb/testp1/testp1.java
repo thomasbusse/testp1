491ce7193924bd5bce86346f2d1ababcb578c93e
tbusse <tbusse@bod00007.internal.novero.com>
2010-02-10 13:22:35 +0100

    initial commit

---
+++ src/com/tb/testp1/testp1.java
@@ -0,0 +1,13 @@
+package com.tb.testp1;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class testp1 extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
